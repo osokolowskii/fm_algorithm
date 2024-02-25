702aaa7dcf434ca7c1c57bac33b5ff970fc39c9a
sokolowski.oskar <61028102+osokolowskii@users.noreply.github.com>
2024-02-25 15:02:18 +0100

    ready function for preparing list of positions

--- strength_algorithm.py
+++ strength_algorithm.py
@@ -52,13 +52,31 @@ class FMAlgorithm:
     #     return roles
 
     def get_positions_list(self, player_positions):
-        positions = player_positions.split('/')
-        if positions != player_positions:
-        for pos in positions:
+        positions_list = player_positions.split('/')
+        all_positions = []
+        if len(positions_list):
+            for i, pos in enumerate(positions_list):
+                if '(' not in pos:
+                    all_sides = self.get_sides_for_position(positions_list, i)
+                else:
+                    all_sides = [pos[i] for i in range(pos.find('(')+1, pos.find(')'))]
+                pos = pos.split('(')[0]
+                all_positions.extend([f'{pos} ({side})' for side in all_sides])
+
+        return all_positions
+    
+    def get_sides_for_position(self, player_positions, start_index):
+        for i in range(start_index + 1, len(player_positions)):
+            if '(' in player_positions[i]:
+                index_of_side = player_positions[i].find('(')
+                sides = list(player_positions[i][index_of_side+1:player_positions[i].find(')')])
+                return sides
+        return []
 
     
     def prepare_roles_for_player(self, player_positions):
         positions_list = self.get_positions_list(player_positions)
+        return positions_list
 
 
 
@@ -99,7 +117,7 @@ class FMAlgorithm:
         return best_players
 
 algorithm = FMAlgorithm()
-print(algorithm.prepare_roles_for_player('M (C)'))
+print(algorithm.prepare_roles_for_player('DM / M (C) / AM (RL)'))
 algorithm.calculate_strength_for_team()
 # for position in algorithm.find_best_for_positions(3, ['gkd', 'afa']):
 #     print(position)
